6a661d5ac368733ef5b0303fbf5fa857fae33307
Pablo Cárdenas <pcardenasoliveros@gmail.com>
2016-12-29 17:26:16 -0600

    Relax library dependencies

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule BambooPostmark.Mixfile do
   end
 
   defp deps do
-    [{:bamboo, "~> 0.7.0"},
+    [{:bamboo, "~> 0.5"},
      {:hackney, "~> 1.6"},
      {:poison, ">= 1.5.0"},
      {:plug, "~> 1.0"},
