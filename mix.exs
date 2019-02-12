b4b02c457a5f80edc6bdede85870156d1597484d
Pablo Cárdenas <pcardenasoliveros@gmail.com>
2019-02-11 20:06:03 -0600

    Upgrade ex_doc to 0.19

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule BambooPostmark.Mixfile do
      {:poison, ">= 1.5.0", only: :test},
      {:plug, "~> 1.0"},
      {:plug_cowboy, "~> 1.0", only: [:test, :dev]},
-     {:ex_doc, "~> 0.13", only: :dev}]
+     {:ex_doc, "~> 0.19", only: :dev}]
   end
 
   defp package do
