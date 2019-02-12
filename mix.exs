39839dcb76071addb01dfa556021375b5ec0a2e3
Pablo Cárdenas <pcardenasoliveros@gmail.com>
2019-02-11 19:49:08 -0600

    Release 0.5.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BambooPostmark.Mixfile do
 
   def project do
     [app: :bamboo_postmark,
-     version: "0.4.2",
+     version: "0.5.0",
      elixir: "~> 1.2",
      source_url: @project_url,
      homepage_url: @project_url,
